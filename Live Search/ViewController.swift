e4fe1e6e2d5ffd74c86adee289389521b7e1a4f8
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-19 21:16:20 -0600

    Initial Commit

---
+++ Live Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Live Search
+//
+//  Created by Charles Martin Reed on 1/19/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
